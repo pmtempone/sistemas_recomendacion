c165e6738a7060cc439e57ef7fc1dd7a5b11d96c
pmtempone <pmtempone@gmail.com>
2017-07-22 11:55:04 -0300

    tabla restaurantes_v2

--- 07_text_to_columns.py
+++ 07_text_to_columns.py
@@ -22,23 +22,41 @@ recomendado['cleaned'] = np.where(resto_recomendado['recomendado_para'].str.cont
 subs = pd.DataFrame()
 
 
-subs = subs.where[pd.isnull(resto_recomendado['recomendado_para'].str.find('Ir en pareja'))==False,1]
 
-subs['Ir en pareja']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Ir en pareja'))==False,1,0)
-subs['Ir con amigos']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Ir con amigos'))==False,1,0)
-subs['Comer con buenos tragos']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Comer con buenos tragos'))==False,1,0)
-subs['Llevar extranjeros']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Llevar extranjeros'))==False,1,0)
-subs['Escuchar música']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Escuchar música'))==False,1,0)
-subs['Comer sin ser visto']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Comer sin ser visto'))==False,1,0)
-subs['Comer al aire libre']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Comer al aire libre'))==False,1,0)
-subs['Comer solo']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Comer solo'))==False,1,0)
-subs['Reunión de negocios']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Reunión de negocios'))==False,1,0)
-subs['Salida de amigas']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Salida de amigas'))==False,1,0)
-subs['Comer bien gastando poco']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Comer bien gastando poco'))==False,1,0)
-subs['Ir con la familia']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Ir con la familia'))==False,1,0)
-subs['Comer tarde']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Comer tarde'))==False,1,0)
-subs['Comer sano ']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Comer sano '))==False,1,0)
-subs['Merendar']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Merendar'))==False,1,0)
-subs['Comer mucho']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Comer mucho'))==False,1,0)
-subs['Ir con chicos']= np.where(pd.isnull(resto_recomendado['recomendado_para'].str.find('Ir con chicos'))==False,1,0)
+subs['Ir en pareja']= np.where((resto_recomendado['recomendado_para'].str.find('pareja')>=0)==True,1,0)
+subs['Ir con amigos']= np.where((resto_recomendado['recomendado_para'].str.find('amigos')>=0)==True,1,0)
+subs['Comer con buenos tragos']= np.where((resto_recomendado['recomendado_para'].str.find('tragos')>=0)==True,1,0)
+subs['Llevar extranjeros']= np.where((resto_recomendado['recomendado_para'].str.find('extranjeros')>=0)==True,1,0)
+subs['Escuchar música']= np.where((resto_recomendado['recomendado_para'].str.find('música')>=0)==True,1,0)
+subs['Comer sin ser visto']= np.where((resto_recomendado['recomendado_para'].str.find('visto')>=0)==True,1,0)
+subs['Comer al aire libre']= np.where((resto_recomendado['recomendado_para'].str.find('libre')>=0)==True,1,0)
+subs['Comer solo']= np.where((resto_recomendado['recomendado_para'].str.find('solo')>=0)==True,1,0)
+subs['Reunión de negocios']= np.where((resto_recomendado['recomendado_para'].str.find('negocios')>=0)==True,1,0)
+subs['Salida de amigas']= np.where((resto_recomendado['recomendado_para'].str.find('amigas')>=0)==True,1,0)
+subs['Comer bien gastando poco']= np.where((resto_recomendado['recomendado_para'].str.find('poco')>=0)==True,1,0)
+subs['Ir con la familia']= np.where((resto_recomendado['recomendado_para'].str.find('familia')>=0)==True,1,0)
+subs['Comer tarde']= np.where((resto_recomendado['recomendado_para'].str.find('tarde')>=0)==True,1,0)
+subs['Comer sano ']= np.where((resto_recomendado['recomendado_para'].str.find('sano ')>=0)==True,1,0)
+subs['Merendar']= np.where((resto_recomendado['recomendado_para'].str.find('Merendar')>=0)==True,1,0)
+subs['Comer mucho']= np.where((resto_recomendado['recomendado_para'].str.find('mucho')>=0)==True,1,0)
+subs['Ir con chicos']= np.where((resto_recomendado['recomendado_para'].str.find('chicos')>=0)==True,1,0)
     
+restaurantes_recomendado = pd.concat([restaurantes,subs],axis=1)
+
+
+resto_medios = restaurantes[['id_restaurante','medios_pago']]
+
+medios = pd.DataFrame(resto_medios.medios_pago.str.split(';',expand=True))
+
+s = pd.Series((resto_medios['medios_pago'].str.split(';')))
+
+medios = pd.get_dummies(s.apply(pd.Series).stack()).sum(level=0)
+
+restaurantes_recomendado = pd.concat([restaurantes_recomendado,medios],axis = 1)
+
+restaurantes_recomendado.iloc[:,37:46] = restaurantes_recomendado.iloc[:,37:46].fillna(0)
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://postgres:saga305@localhost:5432/sistemas_recomendacion')
+
+restaurantes_recomendado.to_sql('restaurantes_v2', engine,index=False)
