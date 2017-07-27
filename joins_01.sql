fc95194554763bc4515b9e902e64c332513aa33d
pmtempone <pmtempone@gmail.com>
2017-07-27 20:40:21 -0300

    Testeos

--- joins_01.sql
+++ joins_01.sql
@@ -4,9 +4,4 @@ select a.*,b.edad,substr(b.fecha_alta,1,4) as anio_alta,b.tipo
 from ratings_train a,usuarios b
 where a.id_usuario=b.id_usuario;
 
---creacion de tabla de cantidad de seguidores por usuario
-
-select id_usuario,count(id_usuario_seguido) as cant_seguidores
-into usuarios_seguidores
-from siguiendo
-group by id_usuario;
+--
